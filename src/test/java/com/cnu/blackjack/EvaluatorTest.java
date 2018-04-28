cd30ee2a142e038c4729af08bf7d1f4acd680078
hyunkyung <change970525@gmail.com>
2018-04-29 02:37:22 +0900

    3

--- src/test/java/com/cnu/blackjack/EvaluatorTest.java
+++ src/test/java/com/cnu/blackjack/EvaluatorTest.java
@@ -1,28 +1,15 @@
 package com.cnu.blackjack;
 import org.junit.Test;
 
-import java.util.Map;
-
 public class EvaluatorTest {
     @Test
-    public void 게임초기화시_모든플레이어는_2장의카드를_받는다() {
-        Evaluator evaluator=new Evaluator("asdf",500);
-        evaluator
+    public void 게임시작() {
+        Deck deck=new Deck(1);
+        Game game = new Game(deck);
 
-    }
-
-    @Test
-    public void 각_플레이어는_16이하면_히트한다() {
-
-    }
-
-    @Test
-    public void 블랙잭이나오면_2배로_보상받고_해당_플레이어의_턴은_끝난다() {
-
-    }
-
-    @Test
-    public void 각_플레이어는_17이상이면_스테이한다() {
-
+        game.addPlayer("hello", 5000);
+        game.placeBet("hello", 1000);
+        Evaluator evaluator = new Evaluator("hello",Player);
+        evaluator.start();
     }
 }
